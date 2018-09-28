5dfbb603e7b991e94a92eb85ff1dc2236ce16c61
Frederik Foss Nielsen <frederik2812@gmail.com>
2018-09-28 14:13:52 +0200

    updates

---
+++ P3/Xamarin Course/LearnXamarin/LearnXamarin/LearnXamarin/GreetPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace LearnXamarin
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class GreetPage : ContentPage
+	{
+		public GreetPage ()
+		{
+			InitializeComponent ();
+            slider.Value = 0;
+
+            //if (Device.RuntimePlatform == Device.iOS)
+            //    Padding = new Thickness(0, 20, 0, 0);
+            //else if (Device.RuntimePlatform == Device.Android)
+            //    Padding = new Thickness(10, 20, 0, 0);
+            //else if (Device.RuntimePlatform == Device.UWP)
+            //    Padding = new Thickness(30, 20, 0, 0);
+
+        }
+
+    }
+}
